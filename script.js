27bb85ef9152c3ecaf20c14335f3650578d90df0
samirazazy <samiralazazy@gmail.com>
2021-01-11 16:51:44 +0200

    add datatabel functionality

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
+let arr = [];
 document.addEventListener('DOMContentLoaded', () => {
   let input = document.getElementById('input');
-  let table = document.getElementById('table');
 
   input.addEventListener('change', () => {
     readXlsxFile(input.files[0]).then((data) => {
-      // table.innerHTML = null;
-
-      // table.DataTable({
-      //   dom: 'Bfrtip',
-      //   buttons: ['csv', 'copy', 'pdf', 'print'],
-      // });
-
-      var i = 0;
-      data.map((row) => {
-        i == 0 && generateTableHead(table, row);
-        i > 0 && generateTableRows(table, row);
-        i++;
-      });
+      console.log(data);
+      arr = data;
     });
   });
 });
 
-let generateTableHead = (table, data) => {
-  let thead = table.createTHead();
-  let row = thead.insertRow();
-  for (let key of data) {
-    let th = document.createElement('th');
-    let text = document.createTextNode(key);
-    th.appendChild(text);
-    row.appendChild(th);
-  }
+let convertToDataTable = (arr) => {
+  $('#table').DataTable({
+    data: arr,
+    columnDefs: [
+      {
+        targets: [arr[0].length],
+        data: null,
+      },
+    ],
+    dom: 'Bfrtip',
+    buttons: ['copy', 'csv', 'pdf', 'print'],
+  });
 };
 
-let generateTableRows = (table, data) => {
-  let newRow = table.insertRow(-1);
-  data.map((row) => {
-    let newCell = newRow.insertCell();
-    let newText = document.createTextNode(row);
-    newCell.appendChild(newText);
-  });
+let convert = () => {
+  convertToDataTable(arr);
 };
